064bf010ac236066ad55cd29f44ccf8d3146e008
Julia Bajor <juliabajor@onet.pl>
2024-12-11 12:47:12 +0100

    Initial setup

---
+++ applicationAnimalAdoption/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApplicationanimaladoptionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'applicationAnimalAdoption'
