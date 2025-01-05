4b340b66451de032899c4e4eefaf5cd4aee7a7fd
NataliaMatu <natalia.mat19@gmail.com>
2025-01-06 00:07:50 +0100

    jest ssl

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import AnimalList from "../components/AnimalList"
 
 function Home() {
-    return <AnimalList route="http://127.0.0.1:8000/api/animals/" method="get" />
+    return <AnimalList route="https://127.0.0.1:8000/api/animals/" method="get" />
 }
 
 export default Home;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import fs from 'fs';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('C:/Users/natal/PycharmProjects/Animal_Adoption_App_with_DB/localhost1.key'),  // Ścieżka do pliku klucza
+      cert: fs.readFileSync('C:/Users/natal/PycharmProjects/Animal_Adoption_App_with_DB/localhost1.crt'), // Ścieżka do pliku certyfikatu
+    },
+    port: 5173,
+  },
 })
