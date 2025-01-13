3db312b94d6f00512cf6c6f9d71f2fb14e68c2d5
jb <139858294+juliabaj@users.noreply.github.com>
2025-01-13 18:48:36 +0100

    Update Home.jsx
    obrazek

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import AnimalList from "../components/AnimalList"
 import SearchBar from "../components/SearchBar";
+import tytulowaImage from "../assets/tytulowa.jpg";
 
 function Home() {
     return (
         <div>
             <SearchBar />
+            <img
+                src={tytulowaImage} alt="Tytułowa strona"
+                style={{ width: "100%", maxWidth: "600px", marginLeft: "0", marginRight: "auto", display: "block" }}
+            />
             <AnimalList route="https://127.0.0.1:8000/api/animals/" method="get" />
         </div>
     );
